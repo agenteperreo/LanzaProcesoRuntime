20c01426ed22372bc152174e5099246e763e6d47
Isaac <igarcar@iesnervion.es>
2022-10-06 19:03:29 +0200

    6/10/2022

--- src/Ejercicio7/Proceso2.java
+++ src/Ejercicio7/Proceso2.java
@@ -10,13 +10,14 @@ public class Proceso2 {
         String [] claseA = new String[10];
         String [] claseB = new String[10];
         String [] claseC = new String[10];
+        String [] comprobar = new String[10];
 
         String ip1;
 
         Scanner sc = new Scanner(System.in);
         ip= sc.nextLine();
 
-        ip1=ip.split(".");
+        comprobar=ip.split(".");
     }
 
 }
